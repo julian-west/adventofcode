1813241afa0e7b4a4a5463b43ff6ae54181a6c86
Julian West <westjc@btopenworld.com>
2020-12-07 17:02:46 +0000

    refactor

--- day_7/solution.py
+++ day_7/solution.py
@@ -7,8 +7,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def load_input(file):
-    """Load input rules"""
+def load_input(file: str) -> list:
+    """Load input rules into list of strings"""
 
     with open(file, "r") as f:
         raw_rules = f.read().splitlines()
@@ -16,8 +16,15 @@ def load_input(file):
     return raw_rules
 
 
-def process_rules_into_tuples(raw_rules):
+def create_edge_list(raw_rules: list) -> list:
+    """Create an edge list for the graph
 
+    Converts list of string rules into edge list (list of tuples)
+
+    create_edge_list(["bright indigo bags contain 4 shiny turquoise bags, 3 wavy yellow bags."])
+    >>> [ ('bright indigo','shiny turquoise', 4), ('bright indigo','wavy yellow',3) ]
+
+    """
     rules = []
     for rule in raw_rules:
         parent, child = rule[:-1].split("contain")
@@ -32,15 +39,16 @@ def process_rules_into_tuples(raw_rules):
     return rules
 
 
-def create_rule_graph(rules):
+def create_rule_graph(edge_list: list) -> nx.DiGraph:
+    """Create graph encoding the rules from a list of tuples (edge list)"""
 
     DG = nx.DiGraph()
-    DG.add_weighted_edges_from(rules)
-
+    DG.add_weighted_edges_from(edge_list)
     return DG
 
 
-def count_bags_inside(graph, parent):
+def count_bags_inside(graph: nx.DiGraph, parent: str) -> int:
+    """Recursively count how many bags would be contained within any given bag"""
     total = 0
     for node, edge in graph[parent].items():
         total += edge["weight"] * count_bags_inside(graph, node) + edge["weight"]
@@ -52,7 +60,7 @@ if __name__ == "__main__":
     raw_rules = load_input("input.txt")
 
     # Part 1
-    rules = process_rules_into_tuples(raw_rules)
+    rules = create_edge_list(raw_rules)
     graph = create_rule_graph(rules)
     print(len(nx.ancestors(graph, "shiny gold")))
 
