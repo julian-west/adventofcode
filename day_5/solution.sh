0bddeab83b03a38f829ebb07bef2b897aa713942
Julian West <westjc@btopenworld.com>
2020-12-09 17:03:09 +0000

    bash solution day 5

--- day_5/solution.sh
+++ day_5/solution.sh
@@ -28,15 +28,9 @@ low=$(echo "${seat_ids[@]}" | tail -1)
 # Part 2
 # ------------------------------------------------------
 
-# read file again but save sorted list of seat ids
-val=$(
-        while IFS= read -r line; do
-                echo "$((2#`echo $line | tr FBLR 0101`))";
-        done < input.txt | sort -rn
-)
 # calculate theoretical sum of all seat ids (if none were missing)
 theoretical_sum=$(seq $low $high | paste -sd + - | bc)
-actual_sum=$(echo "${val[@]}" | paste -sd + - | bc)
+actual_sum=$(echo "${seat_ids[@]}" | paste -sd + - | bc)
 
 # your seat id is theoretical sum minus actual sum (as there are no missing seats)
 echo `expr $theoretical_sum - $actual_sum`
