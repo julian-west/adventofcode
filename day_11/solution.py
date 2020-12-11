84968dcc9b60120a68c7e951b002b19a3a3eae6e
Julian West <westjc@btopenworld.com>
2020-12-11 11:55:18 +0000

    day 11 part2

--- day_11/solution.py
+++ day_11/solution.py
@@ -1,6 +1,7 @@
 """
 Day 11 Advent of code
 """
+from typing import Callable
 import numpy as np
 
 
@@ -32,19 +33,49 @@ def get_adjacent_occupied_seats(matrix: np.array, x_pos: int, y_pos: int) -> int
     return occupied
 
 
-def fill_seats(matrix: np.array, max_occ: int = 4) -> np.array:
+def get_visible_neighbours(matrix: np.array, x_pos: int, y_pos: int) -> int:
+    """Get number of occupied seats which are visible from a given position"""
+    occupied = 0
+    for y in range(-1, 2):
+        for x in range(-1, 2):
+            if x == 0 and y == 0:
+                continue
+            occupied += is_seat_visible(matrix, x_pos, y_pos, x, y)
+    return occupied
+
+
+def is_seat_visible(
+    matrix: np.array, x_pos: int, y_pos: int, x_dir: int, y_dir: int
+) -> int:
+    """Checks if a seat is occupied within view"""
+    x = x_pos + x_dir
+    y = y_pos + y_dir
+    while 0 <= x < len(matrix[0]) and 0 <= y < len(matrix):
+        if matrix[y][x] == "#":
+            return 1
+        if matrix[y][x] == "L":
+            return 0
+        x += x_dir
+        y += y_dir
+
+    return 0
+
+
+def fill_seats(
+    matrix: np.array, max_occ: int, neighbour_check_fn: Callable
+) -> np.array:
     """Return iteration of seat filling"""
     new_layout = np.empty(matrix.shape, dtype=str)
     for y in range(len(matrix)):
         for x in range(len(matrix[0])):
             if matrix[y][x] == "L":
-                adj_ocupied = get_adjacent_occupied_seats(matrix, x, y)
+                adj_ocupied = neighbour_check_fn(matrix, x, y)
                 if adj_ocupied == 0:
                     new_layout[y][x] = "#"
                 else:
                     new_layout[y][x] = matrix[y][x]
             elif matrix[y][x] == "#":
-                adj_ocupied = get_adjacent_occupied_seats(matrix, x, y)
+                adj_ocupied = neighbour_check_fn(matrix, x, y)
                 if adj_ocupied >= max_occ:
                     new_layout[y][x] = "L"
                 else:
@@ -60,25 +91,31 @@ def count_total_ocupied_seats(matrix: np.array) -> int:
     return (matrix == "#").sum()
 
 
-def part1(matrix: np.array) -> int:
+def run_simulation(matrix: np.array, max_occ: int, neighbour_check_fn: Callable) -> int:
     """Run simulation until it converges and return count of occupied seats
 
     When the matrices converge, returns the number of iterations and the final
     number of occupied seats
     """
-    counter = 0
     old_matrix = matrix
     while True:
-        new_matrix = fill_seats(old_matrix)
+        new_matrix = fill_seats(old_matrix, max_occ, neighbour_check_fn)
         if np.array_equal(old_matrix, new_matrix):
-            return counter, count_total_ocupied_seats(new_matrix)
+            return count_total_ocupied_seats(new_matrix)
         old_matrix = new_matrix
-        counter += 1
 
 
 if __name__ == "__main__":
     input_matrix = load_input("input.txt")
 
     # Part 1
-    iterations, seat_count = part1(input_matrix)
-    print(iterations, seat_count)
+    p1_seat_count = run_simulation(
+        input_matrix, max_occ=4, neighbour_check_fn=get_adjacent_occupied_seats
+    )
+    print(p1_seat_count)
+
+    # Part 2
+    p2_seat_count = run_simulation(
+        input_matrix, max_occ=5, neighbour_check_fn=get_visible_neighbours
+    )
+    print(p2_seat_count)
