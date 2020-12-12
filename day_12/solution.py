f05f013cbaab27758b7cc2d3c462a8e42dfcc919
Julian West <westjc@btopenworld.com>
2020-12-12 17:28:58 +0000

    day 12 part 2

--- day_12/solution.py
+++ day_12/solution.py
@@ -25,7 +25,7 @@ def find_new_direction(cur_direction: str, instruction: str, magnitude: int) -> 
     return [k for k, v in compass_degrees.items() if v == new_heading][0]
 
 
-def manhattan_distance(positions_dict: Dict):
+def manhattan_distance(positions_dict: Dict[str, int]) -> int:
     """Calculates the Manhattan distance"""
     n_s = abs(positions_dict["N"] - positions_dict["S"])
     e_w = abs(positions_dict["E"] - positions_dict["W"])
@@ -33,7 +33,7 @@ def manhattan_distance(positions_dict: Dict):
     return n_s + e_w
 
 
-def part1(directions: List[Tuple[str, int]], start_direction="E"):
+def part1(directions: List[Tuple[str, int]], start_direction="E") -> int:
     """Returns the Manhattan distance of the final position of the ship"""
     cur_direction = start_direction
     cur_positions = {"N": 0, "S": 0, "E": 0, "W": 0}
@@ -49,8 +49,46 @@ def part1(directions: List[Tuple[str, int]], start_direction="E"):
     return manhattan_distance(cur_positions)
 
 
+def find_new_waypoint(
+    waypoint: Dict[str, int], instruction: str, magnitude: int
+) -> Dict[str, int]:
+    """Find the new position of the waypoint - rotate left or right by magnitude"""
+    compass = ["N", "E", "S", "W"]
+    shift = int((magnitude / 90) % 4)
+
+    if instruction == "R":
+        new_compass_positions = compass[shift:] + compass[:shift]
+    elif instruction == "L":
+        new_compass_positions = compass[-shift:] + compass[:-shift]
+
+    new_waypoint = {}
+    for old_dir, new_dir in zip(compass, new_compass_positions):
+        new_waypoint[new_dir] = waypoint[old_dir]
+
+    return new_waypoint
+
+
+def part2(directions: List[Tuple[str, int]], waypoint: Dict) -> int:
+    """Returns the Manhattan distance of the final position of the ship for part2"""
+    cur_positions = {"N": 0, "S": 0, "E": 0, "W": 0}
+
+    for (instruction, magnitude) in directions:
+        if instruction in ["N", "S", "E", "W"]:
+            waypoint[instruction] += magnitude
+        elif instruction == "F":
+            for direction in cur_positions:
+                cur_positions[direction] += waypoint[direction] * magnitude
+        elif instruction in ["L", "R"]:
+            waypoint = find_new_waypoint(waypoint, instruction, magnitude)
+
+    return manhattan_distance(cur_positions)
+
+
 if __name__ == "__main__":
     input_directions = load_input("input.txt")
 
     # Part 1
     print(part1(input_directions))
+
+    # Part 2
+    print(part2(input_directions, waypoint={"N": 1, "E": 10, "S": 0, "W": 0}))
