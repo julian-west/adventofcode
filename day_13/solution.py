f7c43567d54ff2512dccc6c7871e1b0cfb727175
Julian West <westjc@btopenworld.com>
2020-12-13 13:14:22 +0000

    day 13 part2

--- day_13/solution.py
+++ day_13/solution.py
@@ -1,31 +1,57 @@
 """
 Day 13 adventofcode
 """
-from typing import Tuple, Set
+from typing import Tuple, List
+from functools import reduce
 
 
-def load_input(file: str) -> Tuple[int, Set[int]]:
+def load_input(file: str) -> Tuple[int, List[str]]:
     """Load input file"""
     with open(file, "r") as input_file:
         lines = input_file.read().splitlines()
 
-    earliest_time = int(lines[0])
-    bus_ids = set(int(bus) for bus in lines[1].split(",") if bus != "x")
-    return earliest_time, bus_ids
+    return int(lines[0]), lines[1].split(",")
 
 
-def part1(earliest_time: int, bus_ids: Set[int]) -> int:
+def part1(earliest_time: int, bus_ids: List[str]) -> int:
     """Find the earliest bus you can take
 
     Return the bus ID by the number of minutes you have to wait
     """
     time = earliest_time
+    bus_id_set = set(int(bus) for bus in bus_ids if bus != "x")
     while True:
-        for bus in bus_ids:
+        for bus in bus_id_set:
             if time % bus == 0:
                 return bus * (time - earliest_time)
-
         time += 1
+
+
+def part2(bus_ids: List[str]) -> int:
+    """Find the earliest time that the buses depart sequentially
+
+    This will take too long to run using brute force. Can use the Chinese
+    Remainder Theorem as all the bus ids are prime numbers
+
+    https://brilliant.org/wiki/chinese-remainder-theorem/
+
+    """
+
+    def crt(times: List[int], offsets: List[int]) -> int:
+        """Chinese remainder theorem"""
+        N = reduce(lambda a, b: a * b, times)
+        x = sum(
+            offset * (N // time) * pow(N // time, -1, time)
+            for offset, time in zip(offsets, times)
+        )
+
+        return x % N
+
+    buses = [(i, int(bus)) for i, bus in enumerate(bus_ids) if bus != "x"]
+    offsets = [time - i for i, time in buses]
+    times = [t for _, t in buses]
+
+    return crt(times, offsets)
 
 
 if __name__ == "__main__":
@@ -33,3 +59,6 @@ if __name__ == "__main__":
 
     # Part 1
     print(part1(earliest_time, bus_ids))
+
+    # Part 2
+    print(part2(bus_ids))
--- day_13/test_solution.py
+++ day_13/test_solution.py
@@ -1,18 +1,33 @@
 """
 Tests for day 13
 """
-
+from typing import List
 import pytest
-from solution import part1
+from solution import part1, part2
 
 TEST_INPUT = """
 939
 7,13,x,x,59,x,31,19
 """
-earliest_time = int(TEST_INPUT.split()[0])
-bus_ids = set([int(bus) for bus in TEST_INPUT.split()[1].split(",") if bus != "x"])
+earliest_time, bus_ids = int(TEST_INPUT.split()[0]), TEST_INPUT.split()[1].split(",")
 
 
 def test_part1():
     """Test part1 function"""
     assert part1(earliest_time=earliest_time, bus_ids=bus_ids) == 295
+
+
+@pytest.mark.parametrize(
+    "bus_ids,expected",
+    [
+        (bus_ids, 1068781),
+        (["17", "x", "13", "19"], 3417),
+        (["67", "7", "59", "61"], 754018),
+        (["67", "x", "7", "59", "61"], 779210),
+        (["67", "7", "x", "59", "61"], 1261476),
+        (["1789", "37", "47", "1889"], 1202161486),
+    ],
+)
+def test_part2(bus_ids: List[str], expected: int):
+    """Test part2 function"""
+    assert part2(bus_ids) == expected
