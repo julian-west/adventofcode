90ab4964312828a5b89248b45d1c1050341a7be0
julian-west <47723873+julian-west@users.noreply.github.com>
2020-12-13 14:31:12 +0000

    Update test_solution.py

--- day_13/test_solution.py
+++ day_13/test_solution.py
@@ -5,22 +5,18 @@ from typing import List
 import pytest
 from solution import part1, part2
 
-TEST_INPUT = """
-939
-7,13,x,x,59,x,31,19
-"""
-earliest_time, bus_ids = int(TEST_INPUT.split()[0]), TEST_INPUT.split()[1].split(",")
-
-
 def test_part1():
     """Test part1 function"""
-    assert part1(earliest_time=earliest_time, bus_ids=bus_ids) == 295
+    assert part1(
+        earliest_time = 939,
+        bus_ids = ["7", "13", "x", "x", "59", "x", "31", "19"]
+    ) == 295
 
 
 @pytest.mark.parametrize(
     "bus_ids,expected",
     [
-        (bus_ids, 1068781),
+        (["7", "13", "x", "x", "59", "x", "31", "19"], 1068781),
         (["17", "x", "13", "19"], 3417),
         (["67", "7", "59", "61"], 754018),
         (["67", "x", "7", "59", "61"], 779210),
