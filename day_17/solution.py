83e6913f929ee6004c12d0f8059ec6004d07d2b4
Julian West <westjc@btopenworld.com>
2021-01-13 10:00:24 +0000

    run iteration

--- day_17/solution.py
+++ day_17/solution.py
@@ -16,5 +16,12 @@ def find_neighbours(coords: Tuple, n: int = 3) -> List[Tuple]:
 
 def make_matrix(input_grid: str) -> np.array:
     """Turn input string into a 3d np.array"""
-    matrix = [list(line) for line in input_grid.split()]
-    return np.expand_dims(matrix, axis=0)
+    matrix = np.expand_dims([list(line) for line in input_grid.split()], axis=0)
+    enc_matrix = (matrix == "#").astype(np.int)
+    return enc_matrix
+
+
+# def run_iteration(enc_matrix: np.array) -> np.array:
+#     """Run an iteration and return new matrix"""
+#     pad_matrix = np.pad(enc_matrix, 1)
+#     x, y, z = pad_matrix.shape
