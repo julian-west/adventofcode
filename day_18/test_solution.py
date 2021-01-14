2c3167cf59560499dd5f9645e9cf4fcbebe34f8d
Julian West <westjc@btopenworld.com>
2021-01-14 16:26:05 +0000

    day 18

---
+++ day_18/test_solution.py
@@ -0,0 +1,17 @@
+"""Day 18 tests"""
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "raw_input, expected",
+    [
+        ("2 * 3 + (4 * 5)", 26),
+        ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437),
+        ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240),
+        ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632),
+    ],
+)
+def test_evalute(raw_input, expected):
+    """Test evaluate function"""
+    pass
