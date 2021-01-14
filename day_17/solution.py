6b512a421fa78a71c22ed13668be421c459e37d8
Julian West <westjc@btopenworld.com>
2021-01-14 14:55:49 +0000

    part 1 day 17

--- day_17/solution.py
+++ day_17/solution.py
@@ -1,16 +1,16 @@
 """Day 17 solution"""
 
-from typing import List, Tuple
+from typing import Tuple, Iterable
 from itertools import product
 import numpy as np
 
 
-def find_neighbours(coords: Tuple, n: int = 3) -> List[Tuple]:
+def find_neighbours(coords: Tuple, matrix_shape: Tuple) -> Iterable:
     """Find the neighbour coordinates of a given target coordinate"""
-    for transform in product((-1, 0, 1), repeat=n):
+    for transform in product((-1, 0, 1), repeat=len(matrix_shape)):
         if not all(i == 0 for i in transform):
             nb = tuple(coord + offset for coord, offset in zip(coords, transform))
-            if all(i >= 0 for i in nb):
+            if all(i >= 0 for i in nb) and all(i < s for i, s in zip(nb, matrix_shape)):
                 yield nb
 
 
@@ -21,7 +21,36 @@ def make_matrix(input_grid: str) -> np.array:
     return enc_matrix
 
 
-# def run_iteration(enc_matrix: np.array) -> np.array:
-#     """Run an iteration and return new matrix"""
-#     pad_matrix = np.pad(enc_matrix, 1)
-#     x, y, z = pad_matrix.shape
+def run_iteration(enc_matrix: np.array) -> np.array:
+    """Run an iteration and return new matrix"""
+    pad_matrix = np.pad(enc_matrix, 1)
+    new_matrix = pad_matrix.copy()
+
+    for coord, value in np.ndenumerate(pad_matrix):
+        neighbours = find_neighbours(coord, pad_matrix.shape)
+        neighbours_sum = sum([pad_matrix[nb] for nb in neighbours])
+
+        if value == 1:
+            if neighbours_sum not in [2, 3]:
+                new_matrix[coord] = 0
+        else:
+            if neighbours_sum == 3:
+                new_matrix[coord] = 1
+
+    return new_matrix
+
+
+def part_1(input_grid: str, iterations: int) -> int:
+    """Return number of active cubes after a number of iterations"""
+    matrix = make_matrix(input_grid)
+    for i in range(iterations):
+        matrix = run_iteration(matrix)
+    return np.sum(matrix)
+
+
+if __name__ == "__main__":
+    with open("input.txt") as input_file:
+        input_str = input_file.read()
+
+    # Part 1
+    print(part_1(input_str, iterations=6))
