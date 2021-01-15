f11eca14c41e000987d18599b40bf9ba8048a960
Julian West <westjc@btopenworld.com>
2021-01-15 11:44:40 +0000

    day 18 evaluate

--- day_18/solution.py
+++ day_18/solution.py
@@ -1,1 +1,22 @@
 """Day 18 solution"""
+from typing import List
+
+
+def load_input(filepath: str) -> List:
+    """load input into a list"""
+    with open(filepath, "r") as input_file:
+        data = input_file.read().splitlines()
+
+    return [eq.replace(" ", "") for eq in data]
+
+
+def evaluate(eq: str) -> int:
+    """Evaluate an equation and return the result"""
+    pass
+
+
+if __name__ == "__main__":
+
+    equations = load_input("input.txt")
+
+    print(equations[:5])
