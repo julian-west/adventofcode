54a7076fc1d93a509e00d5065a123e0ed68467df
julian-west <westjc@btopenworld.com>
2021-12-04 15:41:59 +0000

    day 4 python solution

---
+++ 2021/day_04/d4_solution.py
@@ -0,0 +1,119 @@
+"""Day 4 Solutions"""
+from dataclasses import dataclass, field
+from itertools import product
+from typing import Optional
+
+
+@dataclass
+class BingoTracker:
+    row: list[int]
+    col: list[int]
+    hits: list[int] = field(default_factory=list)
+
+
+@dataclass
+class WinnersReport:
+    num_rounds: int
+    index: Optional[int]
+    sum_unmarked: int
+    final_score: int
+
+
+class Board:
+    def __init__(self, board: list[list[int]]):
+        self.bingo = False
+        self.board = board
+        self.dim = len(board[0])
+        self.coordinates = list(product(range(self.dim), range(self.dim)))
+        self.tracker = BingoTracker(row=[0] * self.dim, col=[0] * self.dim)
+
+    def play(self, val: int):
+        coordinates = self._find_coordinates(val)
+        if coordinates:
+            self._update_tracker(val, coordinates[0], coordinates[1])
+            self._check_bingo()
+
+    def _find_coordinates(self, val: int) -> Optional[tuple[int, int]]:
+        """find the coordinates of the value"""
+        for (row, col) in self.coordinates:
+            if self.board[row][col] == val:
+                return row, col
+        return None
+
+    def _update_tracker(self, val: int, row: int, col: int):
+        """Update bingo tracker"""
+        self.tracker.hits.append(val)
+        self.tracker.row[row] += 1
+        self.tracker.col[col] += 1
+
+    def _check_bingo(self):
+        """Check if there is a bingo"""
+        if max(self.tracker.row) == self.dim or max(self.tracker.col) == self.dim:
+            print("BINGO!")
+            self.bingo = True
+
+
+class Game:
+    def __init__(self, boards: list[list[list[int]]], draws: list[int]):
+        self.boards = [Board(board) for board in boards]
+        self.draws = draws
+        self.winners_report: Optional[WinnersReport] = None
+
+    def play(self):
+        for round, val in enumerate(self.draws):
+            for index, board in enumerate(self.boards):
+                board.play(val)
+                if board.bingo:
+                    self.winner = index
+                    self._create_winners_report(round, val)
+                    break
+            else:
+                continue
+            break
+
+    def _create_winners_report(self, round: int, last_number: int):
+        sum_unmarked = self._get_sum_unmarked(self.boards[self.winner])
+        final_score = self._get_final_score(sum_unmarked, last_number)
+        self.winners_report = WinnersReport(
+            num_rounds=round,
+            index=self.winner,
+            sum_unmarked=sum_unmarked,
+            final_score=final_score,
+        )
+
+    def _get_sum_unmarked(self, board: Board) -> int:
+        unmarked = [
+            board.board[row][col]
+            for (row, col) in board.coordinates
+            if board.board[row][col] not in board.tracker.hits
+        ]
+        return sum(unmarked)
+
+    def _get_final_score(self, sum_unmarked: int, last_number: int) -> int:
+        return sum_unmarked * last_number
+
+
+def format_board_input(raw_board: str) -> list[list[int]]:
+    """Clean raw input string into list of lists"""
+    rows = raw_board.split("\n")
+    formatted_rows = []
+    for row in rows:
+        formatted_rows.append([int(num) for num in row.split(" ") if num])
+    return formatted_rows
+
+
+def part_1(boards: list[list[list[int]]], draws: list[int]) -> int:
+    game = Game(boards, draws)
+    game.play()
+    return game.winners_report.final_score
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as input:
+        line_breaks = input.read().strip().split("\n\n")
+
+    draws = [int(num) for num in line_breaks[0].split(",")]
+    boards = list(map(format_board_input, line_breaks[1:]))
+
+    part_1_ans = part_1(boards, draws)
+    print(part_1_ans)
---
+++ 2021/day_04/test_d4_solution.py
@@ -0,0 +1,85 @@
+"""Day 4 Solution Tests"""
+
+import pytest
+from d4_solution import Board, format_board_input, part_1
+
+# fmt:off
+draws = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1]
+# fmt: on
+
+raw_board = (
+    "62  5 77 94 75\n59 10 23 44 29\n93 91 63 51 74\n22 14 15  2 55\n78 18 95 58 57"
+)
+
+boards = [
+    [
+        [22, 13, 17, 11, 0],
+        [8, 2, 23, 4, 24],
+        [21, 9, 14, 16, 7],
+        [6, 10, 3, 18, 5],
+        [1, 12, 20, 15, 19],
+    ],
+    [
+        [3, 15, 0, 2, 22],
+        [9, 18, 13, 17, 5],
+        [19, 8, 7, 25, 23],
+        [20, 11, 10, 24, 4],
+        [14, 21, 16, 12, 6],
+    ],
+    [
+        [14, 21, 17, 24, 4],
+        [10, 16, 15, 9, 19],
+        [18, 8, 23, 26, 20],
+        [22, 11, 13, 6, 5],
+        [2, 0, 12, 3, 7],
+    ],
+]
+
+
+def test_format_board():
+    """Test formatting the raw board input from string to list of lists"""
+    assert format_board_input(raw_board) == [
+        [62, 5, 77, 94, 75],
+        [59, 10, 23, 44, 29],
+        [93, 91, 63, 51, 74],
+        [22, 14, 15, 2, 55],
+        [78, 18, 95, 58, 57],
+    ]
+
+
+def test_dim():
+    assert Board(boards[0]).dim == 5
+
+
+@pytest.mark.parametrize(
+    "board,value,expected",
+    [
+        (boards[0], 22, (0, 0)),
+        (boards[0], 10, (3, 1)),
+        (boards[0], 3, (3, 2)),
+        (boards[0], 99, None),
+    ],
+)
+def test__find_coordinates(board, value, expected):
+    playing_board = Board(board)
+    assert playing_board._find_coordinates(value) == expected
+
+
+@pytest.mark.parametrize(
+    "board,rounds,draws,expected",
+    [
+        (boards[0], 5, draws, [7, 4, 9, 5, 11]),
+        (boards[1], 5, draws, [7, 4, 9, 5, 11]),
+        (boards[1], 11, draws, [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21]),
+    ],
+)
+def test_play(board, rounds, draws: list[int], expected: list[int]):
+    """Check number of hits is correct after X rounds"""
+    playing_board = Board(board)
+    for val in draws[:rounds]:
+        playing_board.play(val)
+    assert playing_board.tracker.hits == expected
+
+
+def test_part_1():
+    assert part_1(boards, draws) == 4512
